8372ff9a8511ffdfb5002f06af166cc465949494
Maxime Cannoodt <Maxime.Cannoodt@UGent.be>
2022-07-11 11:34:53 +0200

    move plugin to separate repo

--- plugin/src/obsidian/PluginSettings.ts
+++
@@ -1,9 +0,0 @@
-export interface PluginSettings {
-	serverUrl: string;
-	selfHosted: boolean;
-}
-
-export const DEFAULT_SETTINGS: PluginSettings = {
-	serverUrl: "https://noteshare.space",
-	selfHosted: false,
-};
