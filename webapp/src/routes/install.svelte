ff70c6a45667c5ed4f7606bffdec4006d9d383e9
Maxime Cannoodt <Maxime.Cannoodt@UGent.be>
2022-07-24 14:09:20 +0200

    add install instructions

--- webapp/src/routes/install.svelte
+++ webapp/src/routes/install.svelte
@@ -3,7 +3,18 @@
 </svelte:head>
 
 <div class="md:py-8 prose max-w-3xl dark:prose-invert">
-	<h1>Installing the plugin</h1>
+	<h1>Installing the Obsidian Plugin</h1>
 
-	<p>Coming soon</p>
+	The plugin and Noteshare.space are currently in beta. Therefore, the plugin is not yet available
+	through the Obsidian community plugins marketplace.
+
+	<h2>Beta testing with BRAT</h2>
+
+	<p>
+		To beta test, you can install the plugin using BRAT (see <a
+			href="https://github.com/TfTHacker/obsidian42-brat#adding-a-beta-plugin) for further
+		instructions">BRAT > Adding a beta plugin</a
+		>). Please report any problems you encounter in the plugin
+		<a href="https://github.com/mcndt/obsidian-note-sharing/issues">bug tracker</a>.
+	</p>
 </div>
