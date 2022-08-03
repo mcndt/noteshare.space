c7160b8ce7e3fda374fea55b0bebd920f84f3f26
Maxime Cannoodt <Maxime.Cannoodt@UGent.be>
2022-08-03 22:12:52 +0200

    fix: :lipstick: fix bold text on index page appearing black in dark mode

--- webapp/src/lib/components/index/tile.svelte
+++ webapp/src/lib/components/index/tile.svelte
@@ -2,7 +2,7 @@
 	<div class="text-2xl font-bold mb-4 text-center">
 		<slot name="title" />
 	</div>
-	<div class="prose md:prose-lg text-center text-zinc-700 dark:text-zinc-300">
+	<div class="md:prose-lg text-center text-zinc-700 dark:text-zinc-300">
 		<slot name="text" />
 	</div>
 </div>
